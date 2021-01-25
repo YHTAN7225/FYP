cfbb5522896687e09af871ff2df1ac31b248db27
TAN YI HOW <TP046625@mail.apu.edu.my>
2021-01-25 17:20:50 +0800

    Add project files.

---
+++ FYP/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using FYP2.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(FYP2.Areas.Identity.IdentityHostingStartup))]
+namespace FYP2.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<FYP2Context>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("FYP2ContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<FYP2Context>();
+            });
+        }
+    }
+}
