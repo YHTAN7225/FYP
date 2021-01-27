c26a983f92cdab268439e9ba5b0270252ff61902
TAN YI HOW <TP046625@mail.apu.edu.my>
2021-01-27 23:24:16 +0800

    added create new user page for admin

--- FYP/Controllers/AdminController.cs
+++ FYP/Controllers/AdminController.cs
@@ -1,13 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using FYP.Data;
+using FYP.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FYP.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly FYPContext _context;
+        private UserManager<IdentityUser> _userManager;
+
+        public AdminController(FYPContext context, UserManager<IdentityUser> userManager)
+        {
+            this._context = context;
+            this._userManager = userManager;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -23,8 +37,39 @@ namespace FYP.Controllers
             return View();
         }
 
-        public IActionResult CreateUser()
+      [ActionName("CreateUser")]
+        public ActionResult CreateUser()
         {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("CreateUser")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreateUserAsync(NewUserViewModel model)
+        {
+            if (model == null)
+            {
+                return BadRequest();
+            }
+
+            if (ModelState.IsValid) {
+                model.admin = _userManager.GetUserId(User);
+                var newUser = new IdentityUser { UserName = model.email, Email = model.email };
+                var result = await _userManager.CreateAsync(newUser, model.password);
+
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(newUser, "User");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+                ViewBag.Success = "A New User has been created!";
+            }
+
+            
             return View();
         }
     }
---
+++ FYP/Models/NewUserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FYP.Models
+{
+    public class NewUserViewModel
+    {
+        [Required]
+        public string email { get; set; }
+
+        public string admin { get; set; }
+
+        [Required]
+        public string password { get; set; }
+    }
+}
