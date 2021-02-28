26528cbb776877ef38a6ff1327817f007a7e7399
TAN YI HOW <TP046625@mail.apu.edu.my>
2021-02-28 14:09:47 +0800

    update link string

--- FYP/Controllers/Constant.cs
+++ FYP/Controllers/Constant.cs
@@ -5,7 +5,7 @@
         public const string AzureConnectionString = "DefaultEndpointsProtocol=https;AccountName=storedocstorage;AccountKey=wbGMdw5TonDvzSUwFCk7y8AP7l9LyhUgUxnR55M4ya4Hmc40KPNKipayLuC3Hasbb1sPlcyAuFccBynW1ERwag==;BlobEndpoint=https://storedocstorage.blob.core.windows.net/;TableEndpoint=https://storedocstorage.table.core.windows.net/;QueueEndpoint=https://storedocstorage.queue.core.windows.net/;FileEndpoint=https://storedocstorage.file.core.windows.net/";
 
         public string GeneratedLinkURL(string LinkId) {
-            return "https://localhost:44326/Home/FileSubmissionViaLinkView?LinkId=" + LinkId;
+            return "https://storedoc.azurewebsites.net/Home/FileSubmissionViaLinkView?LinkId=" + LinkId;
         }
 
 
